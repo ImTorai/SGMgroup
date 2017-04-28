0b3204967a1a392e270819f9c37e79e8db8e76a8
Aiguos <mastem1337@gmail.com>
2017-04-28 09:13:29 +0200

    Updated foldername and files

---
+++ Unity/SolarSystem/Assets/Script/CamMovScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchScript.Gestures;
+using System;
+
+public class CamMovScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Application.runInBackground = true;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+		
+	}
+
+
+    private void OnEnable()
+    {
+        GetComponent<PressGesture>().Pressed += onPressed; 
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<PressGesture>().Pressed -= onPressed;
+    }
+
+
+
+    void onPressed(object s, EventArgs a)
+    {
+        Debug.Log("PRESSED");
+    }
+
+}
